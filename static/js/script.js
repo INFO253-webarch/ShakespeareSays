1c6268aa360da50543f03beb40062bd01599ccfc
Reema Naqvi <reemanaqvi@airbears2-10-142-136-113.AirBears2.1918.Berkeley.EDU>
2015-10-15 12:57:58 -0700

    updated js

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,29 @@
 $("#url_shortener").submit(function(){
 
-	var short_url = "";
+	var long_url = "";
+	var longValue = $("#url_shortener input[name='long_url']").val();
 
+	var short_url = "";
 	var shortValue = $("#url_shortener input[name='short_url']").val();
 
-	if (shortValue ===""){
-		alert("You didn't enter anything!") /*change to us generating a URL*/
+
+/* Checks long url field. If it's empty, doesn't reload the page and gives an alert*/
+	if (longValue ===""){				
+		alert("You didn't enter a URL to be shortened!") /*change to us generating a URL*/
+		return false;
 	}
+
+/* Checks short URL. If blank, generates one. If not a digit AND not an empty string, proceeds */
 	else {
-		if(isNaN(shortValue) === false) {
+		if((isNaN(shortValue) === false) && (shortValue != "")) {
 			alert("Invalid input. You cannot enter in a digit!");
 			return false;
 		}
 		else {
-			short_url = shortValue;
+			short_url = shortValue;     
 		}
 	}
 
-	$("#short_url").html(short_url);
-
+	
 	return true;
 });
